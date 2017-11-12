eb7290f42d259fb13c05fd6dee8d4dce24730b8f
omarnasir <momar.nasir@gmail.com>
2017-11-12 19:46:06 +0200

    errors fixed wohoo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-
-'use strict';
-
 var errorElement = document.querySelector('#errorMsg');
-var video = document.querySelector('video');
+var video = document.querySelector('#video');
 
 // Put variables in global scope to make them available to the browser console.
 var constraints = window.constraints = {
   audio: false,
-  video:{ facingMode: {exact: "environment"} } 
+  video: true
 
 };
 
@@ -30,22 +20,23 @@ function handleSuccess(stream) {
 }
 
 function handleError(error) {
-  if (error.name === 'ConstraintNotSatisfiedError') {
-    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
-        constraints.video.width.exact + ' px is not supported by your device.');
-  } else if (error.name === 'PermissionDeniedError') {
-    errorMsg('Permissions have not been granted to use your camera and ' +
-      'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
-  }
-  errorMsg('getUserMedia error: ' + error.name, error);
+  console.log(error);
+  // if (error.name === 'ConstraintNotSatisfiedError') {
+  //   errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+  //       constraints.video.width.exact + ' px is not supported by your device.');
+  // } else if (error.name === 'PermissionDeniedError') {
+  //   errorMsg('Permissions have not been granted to use your camera and ' +
+  //     'microphone, you need to allow the page access to your devices in ' +
+  //     'order for the demo to work.');
+  // }
+  // errorMsg('getUserMedia error: ' + error.name, error);
 }
 
 function errorMsg(msg, error) {
-  errorElement.innerHTML += '<p>' + msg + '</p>';
-  if (typeof error !== 'undefined') {
-    console.error(error);
-  }
+  // errorElement.innerHTML += '<p>' + msg + '</p>';
+  // if (typeof error !== 'undefined') {
+  //   console.error(error);
+  // }
 }
 
 navigator.mediaDevices.getUserMedia(constraints).
